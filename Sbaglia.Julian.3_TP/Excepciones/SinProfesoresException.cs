5bbfb8da71bff9c8ccf2229869b9229088f28672
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2020-07-22 15:54:05 -0300

    Add files via upload

---
+++ Sbaglia.Julian.3_TP/Excepciones/SinProfesoresException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excepciones
+{
+    public class SinProfesorException : Exception
+    {
+        public SinProfesorException()
+            : base("No hay Profesor para la clase.")
+        {
+
+        }
+    }
+}
+
