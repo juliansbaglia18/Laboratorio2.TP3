2915bbd08f356d7faa97cae7cb05c0451c381ac7
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2020-07-22 15:43:24 -0300

    Add files via upload

---
+++ Sbaglia.Julian.3_TP/Clases Instanciables/Profesor.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClasesAbstractas;
+
+namespace Clases_Instanciables
+{
+    public sealed class Profesor : Universitario
+    {
+        private Queue<Universidad.EClases> clasesDelDia;
+        private static Random random;
+
+        /// <summary>
+        /// Constructor estatico que inicializa random.
+        /// </summary>
+        static Profesor()
+        {
+            Profesor.random = new Random();
+        }
+
+        /// <summary>
+        /// Constructor por defecto que llama a la clase base.
+        /// </summary>
+        public Profesor()
+            : base()
+        {
+            this.clasesDelDia = new Queue<Universidad.EClases>();
+            this._randomClases();
+        }
+
+        /// <summary>
+        /// Constructor parametrizado.
+        /// </summary>
+        /// <param name="id">ID del profesor.</param>
+        /// <param name="nombre">Nombre del profesor.</param>
+        /// <param name="apellido">Apellido del profesor.</param>
+        /// <param name="dni">DNI del profesor.</param>
+        /// <param name="nacionalidad">Nacionalidad del profesor.</param>
+        public Profesor(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : base(id, nombre, apellido, dni, nacionalidad)
+        {
+            this.clasesDelDia = new Queue<Universidad.EClases>();
+            this._randomClases();
+        }
+
+
+        /// <summary>
+        /// Agrega las 2 clases a un profesor.
+        /// </summary>
+        private void _randomClases()
+        {
+            this.clasesDelDia.Enqueue((Universidad.EClases)random.Next(1, 4));
+            this.clasesDelDia.Enqueue((Universidad.EClases)random.Next(1, 4));
+        }
+
+        /// <summary>
+        /// Muestra los datos de un profesor.
+        /// </summary>
+        /// <returns>Los datos de un profesor en formato de caracteres.</returns>
+        protected override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("{0}", base.MostrarDatos());
+            sb.AppendLine(this.ParticiparEnClase());
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Muestra los datos de un profesor.
+        /// </summary>
+        /// <returns>Los datos de un profesor en formato de caracteres.</returns>
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+        /// <summary>
+        /// Muestra las clases que da un profesor.
+        /// </summary>
+        /// <returns>Las clases que da en forma de cadena de caracteres.</returns>
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CLASES DEL DIA: ");
+            foreach (Universidad.EClases item in this.clasesDelDia)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Compara si un profesor da una clase.
+        /// </summary>
+        /// <param name="i">Profesor a comparar</param>
+        /// <param name="clase">Clase a comparar.</param>
+        /// <returns>True si da la clase. False si no.</returns>
+        public static bool operator ==(Profesor i, Universidad.EClases clase)
+        {
+            bool retorno = false;
+
+            foreach (Universidad.EClases item in i.clasesDelDia)
+            {
+                if (item == clase)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Compara si un profesor da una clase.
+        /// </summary>
+        /// <param name="i">Profesor a comparar</param>
+        /// <param name="clase">Clase a comparar.</param>
+        /// <returns>False si da la clase. True si no.</returns>
+        public static bool operator !=(Profesor i, Universidad.EClases clase)
+        {
+            return !(i == clase);
+        }
+    }
+}
