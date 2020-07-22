56b155c21d71d6997fa8cf83f14d9ac6a5e8c7f9
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2020-07-22 16:50:37 -0300

    Add files via upload

---
+++ Sbaglia.Julian.3_TP/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d8c81a7-869c-4238-96e3-ed94a0cc2304")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sbaglia.Julian.3_TP/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Excepciones;
+using Clases_Instanciables;
+using Archivos;
+using ClasesAbstractas;
+using System.Collections.Generic;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class TestUnitarios
+    {
+        /// <summary>
+        /// Testea que se lance la excepcion DniInvalidoException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(DniInvalidoException))]
+        public void DniInvalidoTest()
+        {
+            Alumno alumno = new Alumno(123, "Test", "Unitario", "9999999a", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+        }
+
+
+        /// <summary>
+        /// Testea que se lance la excepcion ArchivosException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArchivosException))]
+        public void ArchivoTextoTest()
+        {
+            string aux;
+            Texto archivoLeer = new Texto();
+            archivoLeer.Leer("C://JornadaFalso.txt", out aux);
+
+        }
+
+        /// <summary>
+        /// Testea que el atributo Alumnos se haya instanciado.
+        /// </summary>
+        [TestMethod]
+        public void InstanciaColeccionTest()
+        {
+            Universidad universidad = new Universidad();
+
+            Assert.IsInstanceOfType(universidad.Alumnos, typeof(List<Alumno>));
+
+        }
+
+    }
+}
